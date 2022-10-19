ba0c517c94033d47476c3ae101c4a11dafd0feb7
Sinan Aumarah <saumarah@atlassian.com>
2022-10-19 11:23:57 +1100

    Fixed osgi imports

--- src/main/kotlin/com/atlassian/pedagogical/config/KotlinConfig.kt
+++ src/main/kotlin/com/atlassian/pedagogical/config/KotlinConfig.kt
@@ -1,22 +1,26 @@
 package com.atlassian.pedagogical.config
 
 import com.atlassian.pedagogical.Foo
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport
 import com.atlassian.plugins.osgi.javaconfig.ExportOptions
 import com.atlassian.plugins.osgi.javaconfig.OsgiServices.exportOsgiService
-import com.atlassian.plugins.osgi.javaconfig.OsgiServices.importOsgiService
 import com.atlassian.sal.api.ApplicationProperties
 import org.osgi.framework.ServiceRegistration
 import org.springframework.beans.factory.FactoryBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-open class KotlinConfig {
-    //TODO fix importing from Kotlin
-//    @Bean
-//    open fun applicationProperties() : ApplicationProperties {
-//        return importOsgiService(ApplicationProperties::class.java)
-//    }
+open class KotlinConfig @Autowired constructor(
+    @ComponentImport private val applicationProperties: ApplicationProperties
+) {
+
+    // This is really redundant as we can just Autowire external services on demand
+    @Bean
+    open fun applicationProperties(): ApplicationProperties {
+        return applicationProperties
+    }
 
     @Bean
     open fun exportFooService(foo: Foo?): FactoryBean<ServiceRegistration<*>?>? {
--- src/main/kotlin/com/atlassian/pedagogical/rest/KotlinRest.kt
+++ src/main/kotlin/com/atlassian/pedagogical/rest/KotlinRest.kt
@@ -1,6 +1,9 @@
 package com.atlassian.pedagogical.rest
 
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed
+import com.atlassian.sal.api.ApplicationProperties
+import org.springframework.beans.factory.annotation.Autowired
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -8,17 +11,27 @@ import javax.ws.rs.core.MediaType
 
 // Don't want two REST resources with the same root path
 @Path("/kotlin")
-class KotlinRest {
+class KotlinRest @Autowired constructor(
+    @ComponentImport private val applicationProperties: ApplicationProperties
+) {
 
     /**
      * Take a look at
      * @see <a href="http://localhost:5990/refapp/rest/kotlin-example/1.0/kotlin">http://localhost:5990/refapp/rest/kotlin-example/1.0/kotlin</a>
      */
-    @AnonymousAllowed
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     @Path("")
+    @AnonymousAllowed
+    @Produces(MediaType.TEXT_PLAIN)
+    fun helloWorld2(): String {
+        return "hey there from kotlin land yay"
+    }
+
+    @GET
+    @Path("info")
+    @AnonymousAllowed
+    @Produces(MediaType.TEXT_PLAIN)
     fun helloWorld(): String {
-        return "hey there from kotlin land"
+        return "buildNumber: ${applicationProperties.buildNumber}, version: ${applicationProperties.version}"
     }
 }
