84c8370bf72ffba8f2667bf295bac18409c13907
Gatlee Kaw <gkaw@atlassian.com>
2022-10-20 15:44:32 +1100

    AUJM1-51: Add rudimentary plugin settings

--- src/main/kotlin/com/atlassian/pedagogical/rest/KotlinRest.kt
+++ src/main/kotlin/com/atlassian/pedagogical/rest/KotlinRest.kt
@@ -3,17 +3,22 @@ package com.atlassian.pedagogical.rest
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed
 import com.atlassian.sal.api.ApplicationProperties
+import com.atlassian.sal.api.pluginsettings.PluginSettings
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory
 import org.springframework.beans.factory.annotation.Autowired
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+
 
 // Don't want two REST resources with the same root path
 @Path("/kotlin")
 class KotlinRest @Autowired constructor(
-    @ComponentImport private val applicationProperties: ApplicationProperties
+    @ComponentImport private val applicationProperties: ApplicationProperties,
+    @ComponentImport private val pluginSettingsFactory: PluginSettingsFactory
 ) {
+    val pluginSettings: PluginSettings = pluginSettingsFactory.createGlobalSettings()
+    val PLUGIN_SETTINGS_KEY = "com.atlassian.pedagogical:plugin-settings-key"
 
     /**
      * Take a look at
@@ -33,5 +38,22 @@ class KotlinRest @Autowired constructor(
     @Produces(MediaType.TEXT_PLAIN)
     fun helloWorld(): String {
         return "buildNumber: ${applicationProperties.buildNumber}, version: ${applicationProperties.version}"
+    }
+
+    @GET
+    @Path("setting")
+    @AnonymousAllowed
+    @Produces(MediaType.TEXT_PLAIN)
+    fun getSetting(): String {
+        return pluginSettings.get(PLUGIN_SETTINGS_KEY).toString()
+    }
+
+    @POST
+    @Path("setting")
+    @AnonymousAllowed
+    @Produces(MediaType.TEXT_PLAIN)
+    fun setSetting(@QueryParam("word") word: String) : Response? {
+        pluginSettings.put(PLUGIN_SETTINGS_KEY, word)
+        return Response.ok(word).build()
     }
 }
